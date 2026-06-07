230331330301469d61fffbbfe8527572f099ed25
Prajwal P Doddamani <prajwalcsejit@gmail.com>
2026-06-07 12:17:24 +0530

    Add files via upload

---
+++ Assignments Folder/Console Challenge/calculator.js
@@ -0,0 +1,17 @@
+// Addition
+let a = 10;
+let b = 5;
+
+console.log("Addition:", a + b);
+
+// Subtraction
+console.log("Subtraction:", a - b);
+
+// Multiplication
+console.log("Multiplication:", a * b);
+
+// Division
+console.log("Division:", a / b);
+
+// Modulus
+console.log("Modulus:", a % b);
